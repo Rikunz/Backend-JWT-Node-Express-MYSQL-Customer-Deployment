9a06f5ad64b5cb8e39006b83fcbffac8b0890103
Rikunz <thoriqfirdaus8@gmail.com>
2024-06-03 09:18:53 +0700

    update to start container

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,15 @@
-import {Sequelize} from "sequelize";
+import { Sequelize } from "sequelize";
 
-const db = new Sequelize('auth_db','root','',{
-    host: "localhost",
-    dialect: "mysql"
+// Konfigurasi koneksi database
+const db = new Sequelize('auth_db', 'customer', 'Thoriq1414', {
+    host: "34.136.238.133", // ganti dengan IP publik atau private IP Google Cloud SQL Anda
+    dialect: "mysql",
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false // Set to true if you have the CA certificate
+        }
+    }
 });
 
 export default db;
