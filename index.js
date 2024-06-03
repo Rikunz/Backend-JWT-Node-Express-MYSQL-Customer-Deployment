ea9389c2e9222bdb1012fd5a776bb49bbba1fbd2
Rikunz <thoriqfirdaus8@gmail.com>
2024-06-03 12:05:59 +0700

    update

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ try {
     console.error(error);
 }
 
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(cors({ credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
 
-app.listen(5000, () => console.log('Server running at port 5000'));
+app.listen(8080, () => console.log('Server running at port 5000'));
