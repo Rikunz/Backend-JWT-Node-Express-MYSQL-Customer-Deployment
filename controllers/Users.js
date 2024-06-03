4b0cc980825c7eb56d7d1b27cd75f3946ea01818
Rikz <88525167+Rikunz@users.noreply.github.com>
2024-06-03 16:44:40 +0700

    Update Users.js

--- controllers/Users.js
+++ controllers/Users.js
@@ -1,7 +1,6 @@
 import Users from "../models/UserModel.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import { NONE } from "sequelize";
 
 export const getUsers = async (req, res) => {
     try {
