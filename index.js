af29dc4a646ed98c5016015b8d6a6048aaa2da65
Rikunz <thoriqfirdaus8@gmail.com>
2024-06-03 15:04:23 +0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from "cors";
+import Cors from "cors";
 import db from "./config/Database.js";
 import router from "./routes/index.js";
 dotenv.config();
@@ -13,10 +13,17 @@ try {
 } catch (error) {
     console.error(error);
 }
-
-app.use(cors({ credentials: true, origin: "https://project-thoriq.de.r.appspot.com/" }));
-app.use(cookieParser());
+const corsOptions = {
+    origin: "https://testee-zd55fboz5a-uc.a.run.app", // Alamat domain React
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true, // Mengizinkan pengiriman cookies
+    optionsSuccessStatus: 204,
+};
+app.use(Cors(corsOptions));
 app.use(express.json());
+app.use(express.static("/"));
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 app.use(router);
 
-app.listen(8080, () => console.log('Server running at port 5000'));
+app.listen(8080, () => console.log('Server running at port 8080'));
