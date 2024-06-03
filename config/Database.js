d5501e4946e99527a183f004118770bc4d6f65e0
Rikunz <thoriqfirdaus8@gmail.com>
2024-06-03 12:04:33 +0700

    commit

--- config/Database.js
+++ config/Database.js
@@ -2,13 +2,22 @@ import { Sequelize } from "sequelize";
 
 // Konfigurasi koneksi database
 const db = new Sequelize('auth_db', 'customer', 'Thoriq1414', {
-    host: "34.136.238.133",
+    host: "/cloudsql/project-thoriq:us-central1:notes",
     dialect: "mysql",
-    logging: console.log, // enable logging
+    logging: false,
+    // e.g. host: '/cloudsql/my-awesome-project:us-central1:my-cloud-sql-instance'
+    pool: {
+        max: 2,
+        min: 0,
+        acquire: 10000,
+        idle: 0
+    },
     dialectOptions: {
-        connectTimeout: 60000
-    }
+        // e.g. socketPath: '/cloudsql/my-awesome-project:us-central1:my-cloud-sql-instance'
+        // same as host string above
+        socketPath: "/cloudsql/project-thoriq:us-central1:notes"
+    },
+    operatorsAliases: false
 });
-
 
 export default db;
